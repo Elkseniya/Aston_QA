84f8079d8fd1eae4bc2a818110456b8bbc69671f
Dushkova Kseniya <keelita201@gmail.com>
2024-11-17 23:55:46 +0300

    lesson4

---
+++ homework5/src/Main.java
@@ -0,0 +1,71 @@
+public class Main {
+
+
+    public static int sumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Массив должен быть размером 4x4");
+        }
+
+        for (String[] row : array) {
+            if (row.length != 4) {
+                throw new MyArraySizeException("Массив должен быть размером 4x4");
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Ошибка в данных массива в ячейке [" + i + "][" + j + "]: " + array[i][j]);
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        String[][] validArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+
+        String[][] invalidDataArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "x", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+
+        String[][] invalidSizeArray = {
+                {"1", "2", "3"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            System.out.println("Сумма элементов массива: " + sumArray(validArray));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println("Сумма элементов массива: " + sumArray(invalidDataArray));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println("Сумма элементов массива: " + sumArray(invalidSizeArray));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
